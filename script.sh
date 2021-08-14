606a1d39dd1061e882e2262e90044d2f7a08b1b2
Foxtrot47 <jjneutron@outlook.com>
2021-08-14 19:04:01 +0530

    Tune the sleep timers

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@ while true
 do
     user="$( tr -dc A-Za-z0-9 </dev/urandom | head -c 5 )"
     mono MinecraftClient.exe $user "-" HyperOt.aternos.me --Main.autorespawn=true &
-    sleep $( shuf -i 1-2 -n 1 )m
+    sleep $( shuf -i 120-600 -n 1 )s
     sudo pkill -f MinecraftClient.exe
-    sleep $( shuf -i 1-2 -n 1 )m
+    sleep $( shuf -i 1-120 -n 1 )s
 done
